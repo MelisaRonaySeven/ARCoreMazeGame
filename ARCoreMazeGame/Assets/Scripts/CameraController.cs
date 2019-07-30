cf1a904ccc6f5c54926f6005025db6f46dc5cf0f
Melisa Ronay Seven <melisar.seven@gmail.com>
2019-07-30 22:55:48 +0300

    Final version
    Full version of game with colliders.

---
+++ ARCoreMazeGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("TREASURE"))
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+}
